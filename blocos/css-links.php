fc3851cf71886e2ccc553c728ff354e8bdbfffda
Rebecca <rebecca.ignacio@etec.sp.gov.br>
2019-06-25 20:11:23 -0300

    Add files via upload

---
+++ blocos/css-links.php
@@ -0,0 +1,5 @@
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/global.css">
+    
---
+++ blocos/nav.php
@@ -0,0 +1,40 @@
+
+<div class="sidenav">
+  
+  <a href="perfil.php"><i class="fas fa-tools"></i> Perfil</a>
+  <a href="cadastrar-jogo.php"><i class="far fa-address-card"></i> Cadastrar</a>
+  <a href="listar-jogo.php"><i class="fas fa-list"></i> Listar</a>
+  <a href="editar-jogo.php"><i class="fas fa-edit"></i> Editar</a>
+  <a href="" type="button" class="btn-danger" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-sign-out-alt"></i> Sair</a>
+  
+
+
+  
+  
+</div>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Sair?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Deseja sair? Qualquer alteração são salva será perdida.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-times"></i> Fechar</button>
+        <a type="button" href="logout.php" class="btn btn-primary"><i class="fas fa-sign-out-alt"></i> Sair</a>
+      </div>
+    </div>
+  </div>
+</div>
+  
+</div>
+
+
+
